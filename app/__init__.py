79ec570adf6c9ee883beff1d8667b8f9836d1397
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2022-07-31 21:30:23 +0700

    add new url endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
-# from app.tasks.instagram_scraper import ig_profile, ig_user_post, ig_hashtag_post
-# from app.tasks.facebook_scraper import fb_profile, fb_page_post, fb_group_post
-# from flask_cors import CORS
+from flask import Flask, render_template, request
+from app.tasks.instagram import get_insights
+
 
 app = Flask(__name__)
-# CORS(app)
 
-# index
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -15,6 +13,15 @@ def index():
 def login_fb():
     return render_template('login-fb.html')
 
+@app.route('/collect-data', methods=['POST'])
+def collect_data():
+    if request.method == 'POST':
+        auth_response = request.get_json()
+        access_token = auth_response.authResponse['accessToken']
+        get_insights.delay(access_token)
+        return render_template('collect-data.html')
+    
+    return render_template('index.html')
 
 @app.route('/privacy')
 def privacy():
@@ -23,26 +30,3 @@ def privacy():
 @app.route('/tos')
 def tos():
     return render_template('privacy.html')
-# # # get instagram profile
-# @app.route('/instagram/profile/<username>',methods=['GET'])
-# def profileIG(username):
-#     if request.method == 'GET':
-#         ig_profile.delay(username)
-#         return 'scraping instagram profile {} is in progress'.format(username)
-
-
-# # get instagram profile post
-# @app.route('/instagram/post/<username>', methods=['GET'])
-# def profilePostIG(username):
-#     if request.method == 'GET':
-#         ig_user_post.delay(username)
-#         return 'scraping instagram posts of {} is in progress.'.format(username)
-
-
-# # get instagram hashtag post
-# @app.route('/instagram/post/hashtag/<hashtag_name>', methods=['GET'])
-# def hashtagPostIG(hashtag_name):
-#     if request.method == 'GET':
-#         ig_hashtag_post.delay(hashtag_name)
-#         return 'scraping instagram posts of hashtag {} is in progress.'.format(hashtag_name)
-
