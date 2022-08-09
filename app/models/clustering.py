e4f4b9ae4c916f9b58761cfacbb7828af979c205
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2022-08-09 10:09:25 +0700

    clustering process

---
+++ app/models/clustering.py
@@ -0,0 +1,112 @@
+from app.config import IG_POSTS_COLL
+from app.tasks.instagram import mongo
+from app.utils.data_processing import extract_metrics, \
+    aggregate_by_product, casting_int, data_scaling
+
+import pandas as pd
+import pickle
+
+
+def process_instagram_data():
+    """
+    process instagram post and metrics data.
+    output: product_insights in dataframe object
+    """
+    # get instagram post from mongodb
+    post_query = {
+        'id': 1, 'media_type': 1,
+        'timestamp': 1, 'produk_1': 1,
+        'produk_2': 1, 'produk_3': 1, 'produk_4': 1
+    }
+    ig_posts = mongo.getAllDocument('ig-posts', post_query)
+
+    # get instagram media insights from mongodb
+    insight_query = {'data': 1, 'id': 1}
+    media_insights = mongo.getAllDocument(IG_POSTS_COLL, insight_query)
+
+    # create dataframe
+    posts_df = pd.DataFrame(ig_posts, columns=['id', 'media_type', 'timestamp', 'produk_1', 'produk_2', 'produk_3', 'produk_4'])
+    metrics_df = pd.DataFrame(media_insights, columns=['data', 'id'])
+    post_metrics_df = posts_df.merge(metrics_df, how='left', on='id')
+
+    # type casting column timestamp and filter post based on specific time
+    post_metrics_df['timestamp'] = post_metrics_df['timestamp'].astype('datetime64')
+    post_metrics_df['year'] = post_metrics_df['timestamp'].dt.year
+    post_metrics_df['month'] = post_metrics_df['timestamp'].dt.month
+    filtered_metrics = post_metrics_df[(post_metrics_df['year'] == 2022) & (post_metrics_df['month'] < 6)]
+    filtered_metrics.reset_index(inplace=True)
+
+    # extract insights from column data
+    columns_name = [
+        'id', 'engagement', 'impressions', 'reach', 
+        'saved', 'plays', 'shares', 'total_interactions'
+    ]
+    extracted_insights = extract_metrics(filtered_metrics)
+    counted_metrics = pd.DataFrame(extracted_insights, columns=columns_name)
+
+    # join and filter data in specific columns
+    filtered_metrics = filtered_metrics.merge(counted_metrics, how='left', on='id')
+    filtered_metrics.drop(columns='index', inplace=True)
+    filtered_metrics = filtered_metrics[
+        (filtered_metrics['media_type'] != 'VIDEO') & 
+        (filtered_metrics['produk_1'].notna()) & 
+        (filtered_metrics['produk_1'] != '-')
+    ]
+    filtered_metrics.reset_index(inplace=True)
+
+    # aggregate instagram metric data based on product's name
+    column_targets= ['engagement', 'impressions', 'reach', 'saved']
+    df_1 = aggregate_by_product(filtered_metrics, column_targets, 'produk_1')
+    df_2 = aggregate_by_product(filtered_metrics, column_targets, 'produk_2')
+    df_3 = aggregate_by_product(filtered_metrics, column_targets, 'produk_3')
+    df_4 = aggregate_by_product(filtered_metrics, column_targets, 'produk_4')
+    product_insights = pd.concat([df_1, df_2, df_3, df_4], ignore_index=True)
+    product_insights = product_insights.groupby('produk')[column_targets].sum()
+
+    return product_insights
+
+
+def process_sales_data(url):
+    # get sales data
+    sales_df = pd.read_csv(url)
+    sales_df['Produk Dilihat'] = sales_df['Produk Dilihat'].map(casting_int)
+    sales_df['Keranjang'] = sales_df['Keranjang'].map(casting_int)
+    sales_df['Wishlist'] = sales_df['Wishlist'].map(casting_int)
+
+    # rename columns
+    columns_rename = {
+        'Nama Produk': 'produk',
+        'Produk Terjual': 'produk_terjual', 
+        'Produk Dilihat': 'produk_dilihat',
+        'Keranjang': 'keranjang',
+        'Pesanan': 'pesanan',
+        'Wishlist': 'wishlist'
+    }
+    sales_df = sales_df.rename(columns=columns_rename)
+
+    # aggregate product sales by 'produk'
+    targets = ['produk_terjual', 'produk_dilihat', 'keranjang', 'wishlist']
+    sales_df = aggregate_by_product(sales_df, targets, 'produk')
+    return sales_df
+
+
+def merge_instagram_and_sales(ig_df, sales_df):
+    # join social media data and sales data
+    product_sales_insights = sales_df.join(ig_df)
+    # remove NaN value inside entire DataFrame
+    if product_sales_insights.isnull().values.any():
+        product_sales_insights.dropna(inplace=True)
+    df_columns = product_sales_insights.columns.value
+    scaled_df = data_scaling(product_sales_insights, df_columns)
+    return scaled_df
+
+
+def load_model_cluster(url):
+    model = pickle.load(open(url), "rb")
+    return model
+
+
+def process_clustering(model, dataframe):
+    # clustering
+    dataframe['cluster'] = model.predict(dataframe)
+    return dataframe
---
+++ app/utils/data_processing.py
@@ -0,0 +1,81 @@
+from sklearn.preprocessing import MinMaxScaler
+import ast
+
+
+scaler = MinMaxScaler()
+
+
+def extract_metrics(df):
+    extracted_insights = []
+    for i, insight in enumerate(df['data']):
+        if type(insight)!=list:
+            insight = ast.literal_eval(insight)
+        
+        engagement_value = 0
+        impression_value = 0
+        reach_value = 0
+        saved_value = 0
+        plays_value = 0
+        shares_value = 0
+        total_interactions_value = 0
+        for metric in insight:
+            # engagement
+            if metric['name'] == 'engagement':
+                engagement_value = metric['values'][0]['value']
+            # impressions
+            if metric['name'] == 'impressions':
+                impression_value = metric['values'][0]['value']
+
+            # reach
+            if metric['name'] == 'reach':
+                reach_value = metric['values'][0]['value']
+            # saved
+            if metric['name'] == 'saved':
+                saved_value = metric['values'][0]['value']
+            # plays
+            try:
+                if metric['name'] == 'plays':
+                    plays_value = metric['values'][0]['value']
+            except KeyError:
+                    plays_value = 0
+            # shares
+            try:
+                if metric['name'] == 'shares':
+                    shares_value = metric['values'][0]['value']
+            except KeyError:
+                    shares_value = 0
+            # total interactions
+            try:
+                if metric['name'] == 'total_interactions':
+                    total_interactions_value = metric['values'][0]['value']
+            except KeyError:
+                    total_interactions_value = 0  
+        data = (
+                df['id'][i], engagement_value, 
+                impression_value, reach_value, saved_value, 
+                plays_value, shares_value, total_interactions_value
+            )
+        extracted_insights.append(data)
+    return extracted_insights
+
+
+def aggregate_by_product(df, column_targets, based_on='produk_1'):
+    agg_df = df.groupby(based_on, as_index=False)[column_targets].sum()
+    if based_on != 'produk':
+        try:
+            agg_df = agg_df.rename(columns={based_on: 'produk'})
+        except KeyError:
+            return agg_df
+    return agg_df
+
+
+def casting_int(x):
+    x = str(x)
+    if ',' in x:
+        return int(x.replace(',', '_'))
+    return int(x)
+
+
+def data_scaling(df, col_name_list):
+    df[col_name_list] = scaler.fit_transform(df[col_name_list])
+    return df
