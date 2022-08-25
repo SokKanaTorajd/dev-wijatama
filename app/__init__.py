e890641644b5993cc234c5e69681ad1e2be05f21
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2022-08-25 20:52:12 +0700

    sort

--- app/__init__.py
+++ app/__init__.py
@@ -223,6 +223,7 @@ def start_process():
 
 @app.route('/hasil-rekomendasi')
 def output_clustering():
+    notif(session['id'])
     dest_folder = 'clustering-results/'
     files = list_blobs(dest_folder)
     newest = max(files, key=lambda x: x[1])
@@ -233,15 +234,16 @@ def output_clustering():
     filtered_cluster = results[results['cluster']==0]
     products = filtered_cluster['produk']
 
-    page, per_page, offset = get_page_args(page_parameter='page',
-                                           per_page_parameter='per_page')
-    total = len(data)
-    pagination_data = set_offset(data, offset=offset, per_page=per_page)
-    pagination = Pagination(page=page, per_page=per_page, total=total,
-                            css_framework='bootstrap4')
-    notif(session['id'])
-    return render_template('results.html', products=products, data=pagination_data,
-                            pagination=pagination, page=page, per_page=per_page)
+    # page, per_page, offset = get_page_args(page_parameter='page',
+    #                                        per_page_parameter='per_page')
+    # total = len(data)
+    # pagination_data = set_offset(data, offset=offset, per_page=per_page)
+    # pagination = Pagination(page=page, per_page=per_page, total=total,
+    #                         css_framework='bootstrap4')
+
+    # return render_template('results.html', products=products, data=pagination_data,
+    #                         pagination=pagination, page=page, per_page=per_page)
+    return render_template('results.html', data=data)
 
 @app.route('/notifikasi', methods=['GET', 'POST'])
 def notify():
